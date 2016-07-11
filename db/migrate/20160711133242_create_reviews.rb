e62769f02eb60edbab69d8f08f0fafd480f35392
Kellye Greene <kellye@gust.com>
2016-07-11 09:44:42 -0400

    ar associations

---
+++ db/migrate/20160711133242_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :guest_id
+      t.integer :reservation_id
+      t.integer :listing_id
+      t.text :description
+      t.integer :rating
+
+      t.timestamps null: false
+    end
+  end
+end
